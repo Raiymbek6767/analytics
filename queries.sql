4928e85029266a6f2651209c58518c707b226647
Raiymbek6767 <wmsnjsmfcf@privaterelay.appleid.com>
2026-06-12 09:02:30 +0500

    Delete queries.sql

--- queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM orders WHERE total_amount > 1000;
-
-SELECT * FROM orders WHERE status='Completed';
-
-SELECT * FROM orders WHERE order_date>'2025-01-01';
-
-SELECT * FROM orders WHERE total_amount BETWEEN 500 AND 1000;
-
-SELECT * FROM orders WHERE status='Cancelled';
